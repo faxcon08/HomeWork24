bec77fa757ecab260d0061e10589c45ec1247dee
faxcon08 <faxcon-08@mail.ru>
2023-02-17 21:35:27 +0300

    First Task 1.1;

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,66 @@
+import java.util.*;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args) {
 
-
+    public static <T> void findMinMax(Stream <? extends T> stream, Comparator<? super T> comparator, BiConsumer <? super T,? super T> minMaxConsumer) {
+        List<T> list;
+        list=stream.sorted(comparator).collect(Collectors.toList());
+        T min=null;
+        T max=null;
+        if (list.size()!=0){
+            if(list.size()==1)
+                min = max = list.get(0);
+            else{
+                min= list.get(0);
+                max = list.get(list.size() - 1);
+            }
+        }
+        minMaxConsumer.accept(min,max);
     }
-}
+    public static void main(String[] args) {
+        Consumer<String> printTask = (x)->
+        {String separator = " ====================== ";System.out.println(separator+x+separator);};
+
+        Integer[] integerMass = {5,7,1,3};
+        Double[] doubleMass = {7.1, 2.3, 5.7, 1.1};
+        Stream <Integer> stream = Arrays.stream(integerMass);
+        Comparator<Integer> integerComparator = new Comparator <Integer>(){
+            @Override
+            public int compare(Integer a1, Integer a2) {
+                if(a1>a2)
+                    return 1;
+                if(a1<a2)
+                    return -1;
+                return 0;
+            }
+        };
+        Comparator<Double> doubleComparator = new Comparator <Double>(){
+            @Override
+            public int compare(Double a1, Double a2) {
+                if(a1>a2)
+                    return 1;
+                if(a1<a2)
+                    return -1;
+                return 0;
+            }
+        };
+        BiConsumer<Integer,Integer> biConsumer = (x,y) ->{System.out.println("Min: "+x +" Max:"+y);};
+        BiConsumer<Double,Double> doubleDoubleBiConsumer = (x,y)->System.out.println("Min: "+x+" Max: "+y);
+        printTask.accept("First Task");
+
+        System.out.println(Arrays.asList(integerMass));
+        findMinMax(stream,integerComparator,biConsumer);
+        System.out.println();
+        System.out.println(Arrays.asList(doubleMass));
+        findMinMax(Arrays.stream(doubleMass),doubleComparator,doubleDoubleBiConsumer);
+
+    }// main
+}// Main
